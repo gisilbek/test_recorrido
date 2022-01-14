05e5537ddffdc64aa6c2c218214e14ec07e8ed30
Gisilbek <gisilbek.parra@aicore.com>
2022-01-14 11:30:05 -0400

    Corriendo test del modelo Contract

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -1,2 +1,7 @@
 class Contract < ApplicationRecord
+  validates :name, presence: true
+  validates :week_start_time, presence: true
+  validates :week_end_time, presence: true
+  validates :weekend_start_time, presence: true
+  validates :weekend_end_time, presence: true
 end
