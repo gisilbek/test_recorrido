023c7a384f85c7fda7faa82b81f60106a88a5d92
Gisilbek <gisilbek.parra@aicore.com>
2022-01-14 08:46:02 -0400

    Coriendo migraciones e instalando gem

--- db/migrate/20220110042035_create_contracts.rb
+++ db/migrate/20220110042035_create_contracts.rb
@@ -1,9 +1,11 @@
 class CreateContracts < ActiveRecord::Migration[5.2]
   def change
     create_table :contracts do |t|
-      t.string :name
-      t.time :start_time
-      t.time :end_time
+      t.string :name, null: false, unique: true
+      t.integer :week_start_time,null: false
+      t.integer :week_end_time, null: false
+      t.integer :weekend_start_time, null: false
+      t.integer :weekend_end_time, null: false
 
       t.timestamps
     end
--- db/migrate/20220110042044_create_events.rb
+++ db/migrate/20220110042044_create_events.rb
@@ -1,12 +1,12 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-      t.string :name
-      t.date :date
-      t.time :start_time
-      t.time :end_time
-      t.references :contrato, foreign_key: true
-      t.enum :engineer
+      t.string :name, null: false
+      t.date :date, null: false
+      t.integer :personal, null: false
+      t.references :contract, foreign_key: true
+      t.time :start_time, null: false
+      t.time :end_time, null: false
 
       t.timestamps
     end
